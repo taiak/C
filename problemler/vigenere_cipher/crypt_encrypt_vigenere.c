23630e449190dd4ba4d7d3e5c79ee59fce09e0c8
Taha Yasir Kıroğlu <yasir.kiroglu@bil.omu.edu.tr>
2017-01-21 19:50:27 +0200

    Create crypt_encrypt_vigenere.c
    vigenere şifreleme çözümü yüklendi

---
+++ problemler/vigenere_cipher/crypt_encrypt_vigenere.c
@@ -0,0 +1,130 @@
+#include <stdio.h>
+#include <ctype.h>  /* for toupper and isalpha */
+#include <string.h> /* for strlen */
+#include <stdlib.h>
+/* 
+fonksiyonlar verilen karakterler alfabeden mi diye bakar alfabeden değilse
+boşluk karakteri dönerler. Alfabedense ve harfleri büyütürler. ve en son harfin
+Büyük halinin şifrelenmiş halini veya özümlenmiş halini dönerler
+*/
+
+//şifreleme fonksiyonu
+int encrypt(int chr1, int chr2){
+	int sum;
+	if(isalpha (chr1) && isalpha (chr2)){ /*if chr1 and chr2 alpha*/
+		chr1 = toupper (chr1);
+		chr2 = toupper (chr2);
+		sum = ( (chr1 + chr2 - 129) % 26) + 64;
+	} else 
+		sum = ' '; /* if chr 1 and chr2 not alpha make it space*/
+	return sum;
+}
+/* şifre çözümleme fonksiyonu */
+/* verilen formüller, soruda verilen tablonun formülize edilmesiyle ortaya çıkmıştır */
+int decrypt(int crypt, int cipher){
+	int sum;
+	if(isalpha (crypt) && isalpha (cipher)){
+		crypt = toupper (crypt);
+		cipher = toupper (cipher);
+		sum = ( (27 + crypt - cipher ) % 26) + 64;
+	} else 
+		sum = ' '; /* if chr 1 and chr2 not alpha make it space*/
+	return sum;
+}
+/* verilen dizinin wordlen uzunluğundaki kısmını şifreler veya çözümler*/
+/* flag = 0 için şifreler, flag = 1 için şifreyi çözümler */
+/* flag = 0 for encrypte, flag = 1 for decrypte */
+
+int encrypt_from_table(int chr, int password){
+	
+	char vigenere[26][26] =
+	{
+	{'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'},
+	{'B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A'},
+	{'C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B'},
+	{'D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C'},
+	{'E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D'},
+	{'F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E'},
+	{'G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F'},
+	{'H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G'},
+	{'I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H'},
+	{'J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I'},
+	{'K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J'},
+	{'L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K'},
+	{'M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L'},
+	{'N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M'},
+	{'O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N'},
+	{'P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O'},
+	{'Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P'},
+	{'R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q'},
+	{'S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R'},
+	{'T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S'},
+	{'U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T'},
+	{'V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U'},
+	{'W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V'},
+	{'X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W'},
+	{'Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X'},
+	{'Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y'},
+	};
+
+	return vigenere[password-65][chr-65];
+}
+int decrypt_from_table(int password, int chipher){
+	
+	char vigenere[26][26] =
+	{
+	{'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'},
+	{'B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A'},
+	{'C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B'},
+	{'D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C'},
+	{'E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D'},
+	{'F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E'},
+	{'G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F'},
+	{'H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G'},
+	{'I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H'},
+	{'J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I'},
+	{'K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J'},
+	{'L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K'},
+	{'M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L'},
+	{'N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M'},
+	{'O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N'},
+	{'P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O'},
+	{'Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P'},
+	{'R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q'},
+	{'S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R'},
+	{'T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S'},
+	{'U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T'},
+	{'V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U'},
+	{'W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V'},
+	{'X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W'},
+	{'Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X'},
+	{'Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y'},
+	};
+	char *finish = strchr(vigenere[password-65], chipher) ;
+	char *start = vigenere[password-65];
+	int coordinate = finish - start;	
+
+	return vigenere[0][coordinate];
+}
+void crypter(char *word, char *cipher, int wordlen, char flag){
+	int i;
+	if(flag == 0)
+		for(i = 0 ; i < wordlen ; i++, word++, cipher++)
+			*word = encrypt_from_table (*word, *cipher);
+	else
+		for(i = 0 ; i < wordlen ; i++, word++, cipher++)
+			*word = decrypt_from_table (*cipher, *word);		
+}
+int main(){
+
+
+	char word[] = "ATTACKATDAWN";
+	char key[] = "LEMONLEMONLE";
+	printf ("Word\t\t:\t%s\n", word);
+	printf ("Key phrase\t:\t%s\n", key);
+	crypter(word, key, strlen(word), 0);
+		printf ("Ciphertext\t:\t%s\n", word);
+	crypter(word, key, strlen(word), 1);	
+		printf ("Encrypted text  :\t%s\n", word);
+	return 0;
+}
